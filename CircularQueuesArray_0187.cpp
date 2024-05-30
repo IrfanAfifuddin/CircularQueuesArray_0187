8fa67342aed4f36b77c511a09cb09f03ba897c51
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-05-30 10:42:20 +0700

    membuat kondisi else

--- CircularQueuesArray_0187.cpp
+++ CircularQueuesArray_0187.cpp
@@ -29,6 +29,15 @@ public:
 		FRONT = 0;
 		REAR = 0;
 	}
+
+	else {
+		// jika REAR berada di posisi terakhir array, kembali ke awal array
+		if (REAR == MAX - 1)
+			REAR = 0;
+		else 
+			REAR = REAR + 1
+	}
+	queue_array[REAR] = num;
 };
 
 int main()
