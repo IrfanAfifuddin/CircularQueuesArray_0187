bf5089a964395103dcee7ff6c986b1d121527dba
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-05-30 10:47:29 +0700

    fix error

--- CircularQueuesArray_0187.cpp
+++ CircularQueuesArray_0187.cpp
@@ -12,32 +12,39 @@ public:
 	}
 
 	void insert() {
+
 		int num;
 		cout << "Enter a number :";
 		cin >> num;
 		cout << endl;
-	}
 
-	// cek kenapa antrian sudah penuh
-	if ((FRONT == 0 && REAR == MAX - 1) || (FRONT == REAR + 1)) {
-		cout << "\nQueue overflow\n";
-		return;
-	}
 
-	// cek apakah antrian kosong
-	if (FRONT == -1) {
-		FRONT = 0;
-		REAR = 0;
-	}
+		// cek kenapa antrian sudah penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
 
-	else {
-		// jika REAR berada di posisi terakhir array, kembali ke awal array
-		if (REAR == MAX - 1)
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
 			REAR = 0;
-		else 
-			REAR = REAR + 1
+		}
+
+		else {
+			// jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
-	queue_array[REAR] = num;
+
+	void remove() {
+
+	}
+
 };
 
 int main()
